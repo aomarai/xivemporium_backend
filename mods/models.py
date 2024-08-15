473617572c1791e39628f8998370b2d3583ed97c
Ash Omaraie <49348213+aomarai@users.noreply.github.com>
2024-08-14 19:23:11 -0500

    Setting AWS S3 storage as the default storage provider

--- mods/models.py
+++ mods/models.py
@@ -33,7 +33,7 @@ class Mod(models.Model):
     version = models.CharField(max_length=20, default="1.0.0")
     upload_date = models.DateTimeField(auto_now_add=True)
     file = models.FileField(upload_to="mods/")
-    file_size = models.CharField(max_length=20)
+    file_size = models.CharField(max_length=40)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     downloads = models.IntegerField(default=0)
     categories = models.ManyToManyField(Category, related_name="mods")
