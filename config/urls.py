c52bc491cf687a5a557f701d5b7d9f32654ded2c
Ash Omaraie <49348213+aomarai@users.noreply.github.com>
2024-08-25 22:45:09 -0500

    Adding views and URLs for mod updating and deleting

--- config/urls.py
+++ config/urls.py
@@ -24,9 +24,12 @@ from mods.views import (
     ModSearchByTagAPIView,
     ModSearchByTitleAPIView,
     ModSearchByUserAPIView,
-    ModSearchByRaceAPIView,
-    ModSearchByGenderAPIView,
     ModCreateAPIView,
+    TagListAPIView,
+    RaceListAPIView,
+    GenderListAPIView,
+    ModUpdateAPIView,
+    ModDeleteAPIView,
 )
 
 BASE_MODS_URL = "mod"
@@ -36,12 +39,15 @@ urlpatterns = [
     path(BASE_MODS_URL, ModListAPIView.as_view(), name="list"),
     path(f"{BASE_MODS_URL}/create/", ModCreateAPIView.as_view(), name="create"),
     path(f"{BASE_MODS_URL}/<uuid:uuid>/", ModDetailAPIView.as_view(), name="detail"),
+    path(f"{BASE_MODS_URL}/<uuid:uuid>/update/", ModUpdateAPIView.as_view(), name="update"),
+    path(f"{BASE_MODS_URL}/<uuid:uuid>/delete/", ModDeleteAPIView.as_view(), name="delete"),
     path(
         f"{BASE_MODS_URL}/category/<int:category_id>/", ModSearchByCategoryAPIView.as_view(), name="search-by-category"
     ),
     path(f"{BASE_MODS_URL}/tag/", ModSearchByTagAPIView.as_view(), name="search-by-tag"),
     path(f"{BASE_MODS_URL}/title/<str:title>/", ModSearchByTitleAPIView.as_view(), name="search-by-title"),
     path(f"{BASE_MODS_URL}/user/<int:user_id>/", ModSearchByUserAPIView.as_view(), name="search-by-user"),
-    path(f"{BASE_MODS_URL}/race", ModSearchByRaceAPIView.as_view(), name="search-by-race"),
-    path(f"{BASE_MODS_URL}/gender", ModSearchByGenderAPIView.as_view(), name="search-by-gender"),
+    path("tags/", TagListAPIView.as_view(), name="tag-list"),
+    path("races/", RaceListAPIView.as_view(), name="race-list"),
+    path("genders/", GenderListAPIView.as_view(), name="gender-list"),
 ]
