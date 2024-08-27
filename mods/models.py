afb034445c2a15127b82b0c1d734aaf67a994d58
AOmaraie <ashomaraie@icloud.com>
2024-08-27 16:33:44 -0500

    Making Race and Gender names non-editable in the models

--- mods/models.py
+++ mods/models.py
@@ -43,22 +43,23 @@ class Tag(models.Model):
         max_length=20, unique=True, db_index=True, validators=[MinLengthValidator(2), MaxLengthValidator(20)]
     )
 
-    def clean(self):
+    def save(self, *args, **kwargs):
         self.full_clean()
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
 
 
 class Race(models.Model):
-    name = models.CharField(max_length=120, unique=True, db_index=True)
+    name = models.CharField(max_length=120, unique=True, db_index=True, editable=False)
 
     def __str__(self):
         return self.name
 
 
 class Gender(models.Model):
-    name = models.CharField(max_length=10, unique=True, db_index=True)
+    name = models.CharField(max_length=10, unique=True, db_index=True, editable=False)
 
     def __str__(self):
         return self.name
